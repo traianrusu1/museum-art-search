b9d957555663f325ce1ea1945539eb9e9f460d86
Traian Rusu <traianrusu1@gmail.com>
2020-02-08 13:54:06 -0500

    Added some basic layout and styling

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+	return <nav className="navbar navbar-expand-lg navbar-light bg-light ">Museum Art</nav>;
+};
+
+export default Header;
---
+++ src/components/search/SearchForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchForm = () => {
+	return (
+		<form className="container mt-3">
+			<div className="form-group">
+				<label htmlFor="search-art-text-input"> Search Art </label>
+				<input className="form-control" type="text" id="search-art-text-input" />
+			</div>
+		</form>
+	);
+};
+
+export default SearchForm;
